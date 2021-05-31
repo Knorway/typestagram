16bfa007501ccc54e1bdeb6cf0bc369413adf233
Knorway <rlaxogud921@gmail.com>
2021-05-31 18:04:38 +0900

    배포중: 클라이언트 url 오류 수정중

--- client/api/index.ts
+++ client/api/index.ts
@@ -5,8 +5,11 @@ const client = axios.create();
 export const API_URL =
 	process.env.NODE_ENV === 'production'
 		? 'https://api.typestagram.site'
-		: 'http://localhost:4000';
+		: 'https://api.typestagram.site';
+// : 'http://localhost:4000';
 
 client.defaults.baseURL = API_URL;
+console.log(`------${client.defaults.baseURL}`);
+console.log(`------${API_URL}`);
 
 export default client;
